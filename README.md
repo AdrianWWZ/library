35fcf900daff54b9d7234a7d95de34dfd970b931
Adrian Wong <adrianwongapple@gmail.com>
2024-03-04 17:06:47 +0800

    Added dialog and finished all the javascript needed for now

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Odin Project
 
 ## Library
+
+A library where you can input books with their title, image of book cover, author, number of pages, whether or not you have read it with the Add Book button
+
+## Tailwind Colors (Slate)
+
+https://tailwindcss.com/docs/customizing-colors
--- index.js
+++ index.js
@@ -13,27 +13,22 @@ const myLibrary = [
     pages: 320,
     haveRead: false,
   },
-  {
-    title: "Alex Rider Point Blanc",
-    image: "img/book-cover.jpg",
-    author: "Anthony Horowitz",
-    pages: 320,
-    haveRead: true,
-  },
 ];
 
-function Book(author, title, pages, haveRead) {
+function Book(title, image, author, pages, haveRead) {
   this.author = author;
+  this.image = image;
   this.title = title;
   this.pages = pages;
   this.haveRead = haveRead;
 }
 
-function addBookToLibrary(author, title, pages, haveRead) {
-  const newBook = new Book(author, title, pages, haveRead);
+function addBookToLibrary(title, image, author, pages, haveRead) {
+  const newBook = new Book(title, image, author, pages, haveRead);
+  displayBook(newBook);
 }
 
-myLibrary.forEach((book) => {
+const displayBook = (book) => {
   const bookContainer = document.createElement("div");
   bookContainer.className = "book";
 
@@ -41,12 +36,14 @@ myLibrary.forEach((book) => {
   bookTitle.innerText = book.title;
 
   const bookImage = document.createElement("img");
-  bookImage.src = book.image;
-  bookImage.alt = "Image of book cover";
+  if (book.image) {
+    bookImage.src = book.image;
+    bookImage.alt = "Image of book cover";
+  }
   bookImage.className = "book-image";
 
   const bookAuthor = document.createElement("div");
-  bookAuthor.innerText = book.author;
+  bookAuthor.innerText = `by ${book.author}`;
   bookAuthor.className = "book-author";
 
   const bookPages = document.createElement("div");
@@ -65,4 +62,26 @@ myLibrary.forEach((book) => {
 
   const mainContainer = document.querySelector(".main-content");
   mainContainer.appendChild(bookContainer);
+};
+
+const dialog = document.querySelector("dialog");
+const showDialogButton = document.querySelector(".add-book");
+const closeDialogButton = document.querySelector(".close-button");
+
+showDialogButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeDialogButton.addEventListener("click", () => {
+  dialog.close();
+  const inputTitle = document.querySelector("#title").value;
+  const inputImage = document.querySelector("#image").value;
+  const inputAuthor = document.querySelector("#author").value;
+  const inputPages = document.querySelector("#pages").value;
+  const inputRead = document.querySelector("#haveRead").checked;
+  addBookToLibrary(inputTitle, inputImage, inputAuthor, inputPages, inputRead);
+});
+
+myLibrary.forEach((book) => {
+  displayBook(book);
 });
